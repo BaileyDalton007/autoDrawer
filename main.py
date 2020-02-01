22509d76044d92e0f8ebac4da50d84d838eeda36
Bailey Dalton <baileydalton007@gmail.com>
2020-01-31 23:25:28 -0500

    adds basic X drawing function

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import pyautogui as pygui
 import keyboard
+import time
+
+
+def drawX():
+
+    pygui.moveTo(x1, y1)
+    pygui.dragTo(x2,y2)
+    pygui.moveTo(x1, y2)
+    pygui.dragTo(x2, y1)
+
 
 while True:
-    try:
-        if keyboard.is_pressed('x'):
-            print("hi")
-            break
-    except:
+    if keyboard.is_pressed('x'):
+        x1, y1 = pygui.position()
+        print(x1, y1)
         break
+    
+time.sleep(1)
+
+while True:
+    if keyboard.is_pressed('x'):
+        x2, y2 = pygui.position()
+        print(x2, y2)
+        break
+
+while True:
+    if keyboard.is_pressed('z'):
+        drawX()
+        print('done')
+        break
+
