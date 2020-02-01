ee399ec51c6fdfcb64b1e971cde1662e46bc4c2b
Bailey Dalton <baileydalton007@gmail.com>
2020-02-01 00:34:17 -0500

    Can now draw cells at a certain resolution

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import pyautogui as pygui
 import keyboard
 import time
 
+res = 5
 
-def drawX():
-
-    pygui.moveTo(x1, y1)
-    pygui.dragTo(x2,y2)
-    pygui.moveTo(x1, y2)
-    pygui.dragTo(x2, y1)
-
+def cellDraw():
+    fx = ((x2 - x1) / res)
+    fy = ((y2 - y1) / res)
+    for yi in range(res + 1):
+        if yi != 0:
+            for xi in range(res + 1):
+                if xi != 0:
+                    pygui.moveTo((fx*(xi-1))+x1, (fy*(yi-1))+y1)
+                    pygui.dragTo((fx*(xi-1))+x1, (fy*yi)+y1)
+                    pygui.dragTo((fx*xi)+x1, (fy*yi)+y1)
+                    pygui.dragTo((fx*xi)+x1, (fy*(yi-1))+y1)
+                    pygui.dragTo((fx*(xi-1))+x1, (fy*(yi-1))+y1)
+    pygui.moveTo(x2, y2)
 
 while True:
     if keyboard.is_pressed('x'):
@@ -27,7 +34,7 @@ while True:
 
 while True:
     if keyboard.is_pressed('z'):
-        drawX()
+        cellDraw()
         print('done')
         break
 
